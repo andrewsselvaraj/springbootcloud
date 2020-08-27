53ed624fa691bf35e69ea749e8ff21c9959a7b12
shiju <shiju@LAPTOP-QFQM3362>
2020-08-27 13:47:59 +0530

    Base Version

---
+++ circuitbreaker/src/test/java/efreelearngroup/circuitbreaker/CircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package efreelearngroup.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
